68cfebf3122bd054afa628bf709b6fc46ce46868
kishore <polireddykishore27@gmail.com>
2021-03-14 19:36:42 +0530

    initial commit

---
+++ src/test/java/Jenk/Jenkins.java
@@ -0,0 +1,17 @@
+package Jenk;
+
+import org.testng.annotations.Test;
+
+public class Jenkins {
+
+
+
+        @Test
+
+        public void test1()
+        {
+            System.out.println("Test 1 is integrated");
+        }
+    }
+
+
